e668bdfd16bd6f03b1e6a27b48beaebf085fb260
Wesley Schleumer <wesley.schleumer@gmail.com>
2015-05-29 17:46:33 -0300

    fixxx

--- README.md
+++ README.md
@@ -38,5 +38,8 @@ start "" "main.exe" -username your_nice_username
 
 This will avoid termbox to overwrite `cmd`'s settings(yeah)
 
+# THANKS FOR MAKE MY CODE SUCKS MORE
+
+[Imgur](http://i.imgur.com/i4u7PY2.png)
 
 # NOTICE ME SENPAI
--- main.go
+++ main.go
@@ -51,7 +51,8 @@ type Platform struct {
 }
 
 type UserStatistic struct {
-  CurrentLanguage string `json:"current_language"`
+  PossibleCurrentLanguageBecausePHPProgrammersDoesntKnowsTheDifferenceBetweenBooleanAndFalseThanks interface{} `json:"current_language,omitempty"`
+  CurrentLanguage string
   FocusLevel string `json:"focus_level"`
   FocusPoints float64 `json:"focus_points"`
   Level string `json:"level"`
@@ -179,12 +180,23 @@ func main() {
       }
 
       var statistic UserStatistic
+      //err = json.Unmarshal([]byte(`
+      //  {"name":"schleumer","level":"23.14","focus_level":"15.91","focus_points":2962,"max_streak":11094,"total_days_coded":380,"total_flow_states":981,"time_spent":1596843,"programming_now":false,"current_language":false,"streaking_now":false,"platforms":{"linux":{"percent_work":25.33,"points":810,"time":394999},"mac os x":{"percent_work":0.33,"points":10,"time":9571},"osx":{"percent_work":11.23,"points":359,"time":192277},"windows":{"percent_work":63.11,"points":2020,"time":999996}},"languages":{"ANY":{"level":"0.14","points":2},"AutoHotkey":{"level":"0.05","points":0},"Batch File":{"level":"0.41","points":6},"Build":{"level":"0.00","points":0},"C#":{"level":"0.05","points":0},"C++":{"level":"1.27","points":33},"CSS":{"level":"0.95","points":21},"Clojure":{"level":"0.55","points":9},"CoffeeScript":{"level":"2.73","points":112},"EJS":{"level":"1.14","points":27},"EJS_alternative":{"level":"0.18","points":2},"EJS_default":{"level":"0.23","points":3},"Elixir":{"level":"0.91","points":20},"Erlang":{"level":"0.05","points":0},"Go":{"level":"2.18","points":78},"GoSublime-Go":{"level":"1.95","points":63},"GoSublime-Template":{"level":"0.00","points":0},"Groovy":{"level":"0.00","points":0},"HOCON":{"level":"0.00","points":0},"HTML":{"level":"3.55","points":180},"HTML (Rails)":{"level":"0.36","points":6},"HTMLMustache":{"level":"0.05","points":1},"Haskell":{"level":"0.09","points":1},"JAVA":{"level":"0.41","points":6},"JSON":{"level":"1.41","points":38},"Jade":{"level":"2.68","points":109},"Java":{"level":"0.00","points":0},"JavaProperties":{"level":"0.00","points":0},"JavaScript":{"level":"5.27","points":364},"JavaScript (JSX)":{"level":"0.00","points":0},"JavaScriptNext":{"level":"0.00","points":0},"LESS":{"level":"2.45","points":93},"Lisp":{"level":"0.00","points":0},"LiveScript":{"level":"6.18","points":494},"Lua":{"level":"0.05","points":1},"Markdown":{"level":"2.05","points":68},"PHP":{"level":"5.73","points":424},"Plain":{"level":"0.32","points":5},"Plain Text":{"level":"2.50","points":97},"Play-Routes":{"level":"0.18","points":2},"Play-Scala-Template":{"level":"0.64","points":12},"Play2HtmlRouting":{"level":"0.00","points":0},"Play2Template":{"level":"0.05","points":0},"PowershellSyntax":{"level":"0.18","points":2},"Properties":{"level":"0.05","points":0},"Python":{"level":"1.55","points":44},"RegExp":{"level":"0.00","points":0},"Ruby":{"level":"1.64","points":49},"Ruby Slim":{"level":"0.14","points":2},"Rust":{"level":"0.55","points":10},"SQL":{"level":"0.14","points":1},"Sass":{"level":"0.05","points":0},"Scala":{"level":"7.18","points":651},"Shell-Unix-Generic":{"level":"0.32","points":5},"TypeScript":{"level":"0.09","points":1},"VimL":{"level":"0.00","points":0},"XML":{"level":"0.86","points":17},"YAML":{"level":"0.86","points":18},"configuration":{"level":"0.27","points":3},"laravel-blade":{"level":"2.45","points":93},"reStructuredText":{"level":"0.00","points":0},"yesod-hamlet":{"level":"0.00","points":0}}}
+      //`), &statistic)
       err = json.Unmarshal(body, &statistic)
       if err != nil {
-        HandleMeLikeOneOfYourFrenchGirls(errors.New(fmt.Sprintf("Error on unmarshaling, probably the user %s doesn't exists", username)))
+        //HandleMeLikeOneOfYourFrenchGirls(errors.New(fmt.Sprintf("Error on unmarshaling, probably the user %s doesn't exists [%s]", username, err.Error())))
+        HandleMeLikeOneOfYourFrenchGirls(errors.New(fmt.Sprintf("[%s]", username, err.Error())))
         redraw <- true
         time.Sleep(time.Second * 60)
         continue
+      }
+
+      switch str := statistic.PossibleCurrentLanguageBecausePHPProgrammersDoesntKnowsTheDifferenceBetweenBooleanAndFalseThanks.(type) {
+        case string:
+          statistic.CurrentLanguage = str
+        default:
+          statistic.CurrentLanguage = "" 
       }
 
       userLevel, err := ParseLevel(statistic.Level)
